f38d150610ea3030c982ad376a788cf56f4cb0d5
Simon Fels <simon.fels@canonical.com>
2019-11-21 20:16:28 +0100

    snap: add multi arch support

--- scripts/start-nats.sh
+++ scripts/start-nats.sh
@@ -3,9 +3,23 @@
 # Copyright 2019 Canonical Ltd.  All rights reserved.
 #
 
-if [ ! -e "$SNAP_COMMON"/server/nats.cfg ]; then
-	echo "WARNING: No configuration, not starting"
-	exit 0
+ARCH=
+MACHINE_ARCH="$(uname -m)"
+case "$MACHINE_ARCH" in
+	x86_64)
+		ARCH="amd64"
+		;;
+	aarch64)
+		ARCH="arm64"
+		;;
+	*)
+		echo "Machine architecture $MACHINE_ARCH is not supported"
+		exit 1
+esac
+
+NATS_SERVER="$SNAP/bin/nats-server.$ARCH"
+if [ -e "$SNAP_COMMON"/nats-server.debug ]; then
+    NATS_SERVER="$SNAP_COMMON"/nats-server.debug
 fi
 
-exec "$SNAP"/bin/nats-server -c "$SNAP_COMMON"/server/nats.cfg
+exec "$NATS_SERVER" -c "$SNAP_COMMON"/server/nats.cfg
