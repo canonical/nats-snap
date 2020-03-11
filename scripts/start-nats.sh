be849766192c354d948427495629a79daea21b30
Simon Fels <simon.fels@canonical.com>
2020-03-11 14:01:33 +0100

    Update to latest upstream release 2.1.4 and build thin snaps

--- scripts/start-nats.sh
+++ scripts/start-nats.sh
@@ -1,23 +1,9 @@
 #!/bin/sh -e
 #
-# Copyright 2019 Canonical Ltd.  All rights reserved.
+# Copyright 2020 Canonical Ltd.  All rights reserved.
 #
 
-ARCH=
-MACHINE_ARCH="$(uname -m)"
-case "$MACHINE_ARCH" in
-	x86_64)
-		ARCH="amd64"
-		;;
-	aarch64)
-		ARCH="arm64"
-		;;
-	*)
-		echo "Machine architecture $MACHINE_ARCH is not supported"
-		exit 1
-esac
-
-NATS_SERVER="$SNAP/bin/nats-server.$ARCH"
+NATS_SERVER="$SNAP/bin/nats-server"
 if [ -e "$SNAP_COMMON"/nats-server.debug ]; then
     NATS_SERVER="$SNAP_COMMON"/nats-server.debug
 fi
